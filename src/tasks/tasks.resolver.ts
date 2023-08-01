f65f4722eaefe71307aefded76a7d4846042d928
Jhonatan.garcia <jhonatan.garcia@sofka.com.co>
2023-08-01 12:46:48 -0500

    save

--- src/tasks/tasks.resolver.ts
+++ src/tasks/tasks.resolver.ts
@@ -1,6 +1,7 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { TasksService } from './tasks.service';
 import { Tasks } from './tasks.entity';
+import { CreateTasksInput } from './dto/create-tasks.input';
 
 @Resolver()
 export class TasksResolver {
@@ -9,5 +10,10 @@ export class TasksResolver {
   @Query((returns) => [Tasks])
   async tasks() {
     return await this.tasksServices.findAll();
+  }
+
+  @Mutation((returns) => Tasks)
+  createtasks(@Args('tastInputs') taskInput: CreateTasksInput) {
+    return this.tasksServices.createTask(taskInput);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Tasks } from './tasks.entity';
+import { CreateTasksInput } from './dto/create-tasks.input';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -11,5 +12,10 @@ export class TasksService {
 
   findAll(): Promise<Tasks[]> {
     return this.tasksRepository.find();
+  }
+
+  createTask(tasks: CreateTasksInput): Promise<Tasks> {
+    const newtasks = this.tasksRepository.create(tasks);
+    return this.tasksRepository.save(newtasks);
   }
 }
