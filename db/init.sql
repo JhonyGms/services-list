117d0fdee15028f90afef7f5f99db300a9403eb3
Jhonatan.garcia <jhonatan.garcia@sofka.com.co>
2023-08-01 09:51:01 -0500

    defin TypeORM and Postgres def

---
+++ db/init.sql
@@ -0,0 +1,2 @@
+SELECT 'CREATE DATABASE tasks_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'tasks_db')\gexec
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,23 @@ import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'tasks_db',
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
     }),
     TasksModule,
   ],
